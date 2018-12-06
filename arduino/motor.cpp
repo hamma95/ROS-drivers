d77e9fb864e045944ca55122ef7802a64b65d7de
Mohamed Amin Houidi <34521170+hamma95@users.noreply.github.com>
2018-12-06 13:47:11 +0100

    Update motor.cpp

--- arduino/motor.cpp
+++ arduino/motor.cpp
@@ -1,60 +1,96 @@
-#define motorA1 6    // Marche avant du premier moteur
-#define motorA2 10   // Marche arrière du premier moteur
-#define motorB1 3   // Marche avant du deuxième moteur
-#define motorB2 9     // Marche arrièredu deuxième moteur
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+#define lmotor1 6    // left motor forward
+#define lmotor2 10   // left motor backward
+#define rmotor1 3   // right motor forward
+#define rmotor2 9     // right motor backward
+
+ros::NodeHandle nh;
+
+void rmotor_cb( const std_msgs::Float32& toggle_msg){
+
+	float speed=toggle_msg.data;
 
 
-//Bluetooth (HC-06 JY-MCU) State pin on pin 2 of Arduino
+	if (speed >= 0){
 
 
-//Useful Variables
-
-int vSpeed = 120;   // Default speed, from 0 to 255
-int vSpeed2 = 130;
 
 
-void setup() {
+		analogWrite(rmotor1,speed);
+		analogWrite(rmotor2, 0);
+	}
 
-  // Set pins as outputs:
-  pinMode(motorA1, OUTPUT);
-  pinMode(motorA2, OUTPUT);
-  pinMode(motorB1, OUTPUT);
-  pinMode(motorB2, OUTPUT);
+	if (speed < 0) {
+
+
+		analogWrite(rmotor1, 0);
+		analogWrite(rmotor2,-speed);
+	}
+
+
+
+
+	}
+
+void lmotor_cb( const std_msgs::Float32& toggle_msg){
+	
+	float speed=toggle_msg.data;
+
+
+	if (speed >= 0){
+		analogWrite(lmotor1,speed);
+		analogWrite(lmotor2, 0);
+	}
+
+	if (speed < 0) {
+		analogWrite(lmotor1, 0);
+		analogWrite(lmotor2,-speed);
+	}
+
+
+
+
+	}
+
+
+
+	
+
+
+
+ros::Subscriber<std_msgs::Float32> lwheel_sub("lmotor_cmd", &lmotor_cb );
+ros::Subscriber<std_msgs::Float32> rwheel_sub("rmotor_cmd", &rmotor_cb );
+
+
+void setup(){
+
+	pinMode(lmotor1, OUTPUT);
+    pinMode(lmotor2, OUTPUT);
+    pinMode(rmotor1, OUTPUT);
+    pinMode(rmotor2, OUTPUT);
+	Serial.begin(57600);
+	nh.initNode();
+	nh.subscribe(lwheel_sub);
+	nh.subscribe(rwheel_sub);
+
+	
   
-  Serial.begin(9600);
+
+
+
+
+
+
+
 }
 
-void loop() {
-  //Stop car when connection lost or bluetooth disconnected
- 
-
-  /***********************Forward****************************/
-  //If state is equal with letter 'F', car will go forward!
-  if (state == 'F') {
-    analogWrite(motorA1, vSpeed); analogWrite(motorA2,0);
-    analogWrite(motorB1, vSpeed2);      analogWrite(motorB2, 0);
-  }
-  /***************************Left*****************************/
-  //If state is equal with letter 'L', wheels will turn left
-  else if (state == 'R') {
-    analogWrite(motorA1, vSpeed2);         analogWrite(motorA2,0);
-    analogWrite(motorB1, 0);   analogWrite(motorB2, vSpeed2);
-  }
-  /***************************Right*****************************/
-  //If state is equal with letter 'R', wheels will turn right
-  else if (state == 'L') {
-    analogWrite(motorA1, 0);   analogWrite(motorA2, vSpeed2);
-    analogWrite(motorB1, vSpeed2);   analogWrite(motorB2, 0);
-  }
-  /************************Stop*****************************/
-  //If state is equal with letter 'S', stop the car
-  else if (state == 'S') {
-    analogWrite(motorA1, 0);  analogWrite(motorA2, 0);
-    analogWrite(motorB1, 0);  analogWrite(motorB2, 0);
 
 
 
+void loop(){
 
-  }
+	nh.spinOnce();
+
 }
-
